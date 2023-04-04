3010e06f8c7b9bb39a660b8214150410681cc11f
Maria Koilalou <mariakoilalou@gmail.com>
2023-04-04 14:45:25 +0200

    first commit

---
+++ AI_Lab4/variant1.py
@@ -0,0 +1,73 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the dataset into a pandas DataFrame
+df = pd.read_csv('variant1.csv')
+
+# Convert categorical variables to numerical using one-hot encoding or label encoding
+df = pd.get_dummies(df, columns=['waterfront', 'view', 'condition', 'grade'])
+
+# Split the dataset into training, validation, and test sets
+train, test = train_test_split(df, test_size=0.2, random_state=42)
+train, val = train_test_split(train, test_size=0.2, random_state=42)
+
+# Define the features and target variable
+features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
+            'yr_built', 'yr_renovated', 'zipcode', 'lat', 'long', 'sqft_living15', 'sqft_lot15']
+target = 'price'
+
+# Linear Regression Model
+lr = LinearRegression()
+
+# Train the model on the training data
+lr.fit(train[features], train[target])
+
+# Use the validation set to tune hyper parameters
+lr_pred_val = lr.predict(val[features])
+lr_mse_val = mean_squared_error(val[target], lr_pred_val)
+lr_r2_val = r2_score(val[target], lr_pred_val)
+
+# Evaluate the model using metrics such as Mean Squared Error (MSE), Root Mean Squared Error (RMSE), and R-squared
+lr_pred_test = lr.predict(test[features])
+lr_mse_test = mean_squared_error(test[target], lr_pred_test)
+lr_rmse_test = np.sqrt(lr_mse_test)
+lr_r2_test = r2_score(test[target], lr_pred_test)
+
+# Random Forest Regression Model
+rf = RandomForestRegressor(n_estimators=100, max_depth=20, random_state=42)
+
+# Train the model on the training data
+rf.fit(train[features], train[target])
+
+# Use the validation set to tune hyperparameters
+rf_pred_val = rf.predict(val[features])
+rf_mse_val = mean_squared_error(val[target], rf_pred_val)
+rf_r2_val = r2_score(val[target], rf_pred_val)
+
+# Evaluate the model using metrics such as Mean Squared Error (MSE), Root Mean Squared Error (RMSE), and R-squared
+rf_pred_test = rf.predict(test[features])
+rf_mse_test = mean_squared_error(test[target], rf_pred_test)
+rf_rmse_test = np.sqrt(rf_mse_test)
+rf_r2_test = r2_score(test[target], rf_pred_test)
+
+# Comparison of Metrics
+print('Linear Regression Model:')
+print('Validation Set Metrics:')
+print('MSE:', lr_mse_val)
+print('R-squared:', lr_r2_val)
+print('Test Set Metrics:')
+print('MSE:', lr_mse_test)
+print('RMSE:', lr_rmse_test)
+print('R-squared:', lr_r2_test)
+
+print('\nRandom Forest Regression Model:')
+print('Validation Set Metrics:')
+print('MSE:', rf_mse_val)
+print('R-squared:', rf_r2_val)
+print('Test Set Metrics:')
+print('MSE:', rf_mse_test)
