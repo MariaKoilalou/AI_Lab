d10d3704d98a6b1c064be56828ea3388f3e18b87
Maria Koilalou <mariakoilalou@gmail.com>
2023-04-23 20:52:36 +0200

    lab 4

--- AI_Lab4/variant1.py
+++ AI_Lab4/variant1.py
@@ -4,29 +4,29 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the dataset into a pandas DataFrame
 df = pd.read_csv('variant1.csv')
 
-# Convert categorical variables to numerical using one-hot encoding or label encoding 
-df = pd.get_dummies(df, columns=['waterfront', 'view', 'condition', 'grade'])
+# Label encode categorical variables
+le = LabelEncoder()
+df['waterfront'] = le.fit_transform(df['waterfront'])
+df['view'] = le.fit_transform(df['view'])
+df['condition'] = le.fit_transform(df['condition'])
+df['grade'] = le.fit_transform(df['grade'])
 
 # Split the dataset into training, validation, and test sets
-# Splitting the dataset into
-# training, validation, and test sets means dividing the available data into three separate subsets to be used for
-# different purposes in building a predictive model.
-# The training set is used to train the model, meaning the model
-# learns the relationships between the input features and the target variable. The validation set is used to tune the
-# hyperparameters of the model and prevent overfitting. Finally, the test set is used to evaluate the performance of
-# the model on new, unseen data.
 train, test = train_test_split(df, test_size=0.2, random_state=42)
 train, val = train_test_split(train, test_size=0.2, random_state=42)
 
 # Define the features and target variable
-features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors',
-            'yr_built', 'yr_renovated', 'zipcode', 'lat', 'long', 'sqft_living15', 'sqft_lot15']
+features = ['bathrooms', 'floors', 'waterfront', 'view', 'condition', 'grade', 'yr_built', 'yr_renovated']
 target = 'price'
+
+print(train.columns)
 
 # Linear Regression Model
 lr = LinearRegression()
@@ -62,6 +62,23 @@ rf_mse_test = mean_squared_error(test[target], rf_pred_test)
 rf_rmse_test = np.sqrt(rf_mse_test)
 rf_r2_test = r2_score(test[target], rf_pred_test)
 
+# Define an instance of the SVM model:
+svm = SVR(kernel='rbf')
+
+# Train the SVM model on the training data
+svm.fit(train[features], train[target])
+
+# Use the validation set to tune hyperparameters
+svm_pred_val = svm.predict(val[features])
+svm_mse_val = mean_squared_error(val[target], svm_pred_val)
+svm_r2_val = r2_score(val[target], svm_pred_val)
+
+# Evaluate the model using metrics such as Mean Squared Error (MSE), Root Mean Squared Error (RMSE), and R-squared
+svm_pred_test = svm.predict(test[features])
+svm_mse_test = mean_squared_error(test[target], svm_pred_test)
+svm_rmse_test = np.sqrt(svm_mse_test)
+svm_r2_test = r2_score(test[target], svm_pred_test)
+
 # Comparison of Metrics
 print('Linear Regression Model:')
 print('Validation Set Metrics:')
@@ -80,7 +97,19 @@ print('Test Set Metrics:')
 print('MSE:', rf_mse_test)
 print('RMSE:', rf_rmse_test)
 print('R-squared:', rf_r2_test)
-if lr_mse_test < rf_mse_test:
+
+print('\nSupport Vector Machine Regression Model:')
+print('Validation Set Metrics:')
+print('MSE:', svm_mse_val)
+print('R-squared:', svm_r2_val)
+print('Test Set Metrics:')
+print('MSE:', svm_mse_test)
+print('RMSE:', svm_rmse_test)
+print('R-squared:', svm_r2_test)
+
+if lr_mse_test < rf_mse_test and lr_mse_test < svm_mse_test:
     print('\nLinear Regression Model performed better on the test set with lower MSE')
-else:
+elif rf_mse_test < lr_mse_test and rf_mse_test < svm_mse_test:
     print('\nRandom Forest Regression Model performed better on the test set with lower MSE')
+else:
+    print('\nSupport Vector Machine Regression Model performed better on the test set with lower MSE')
