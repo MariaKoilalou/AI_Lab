55d5f39bf3090f7a635b075c430ed84f7f9d73fd
Maria Koilalou <mariakoilalou@gmail.com>
2023-05-10 14:20:31 +0200

    grid search

--- AI_Lab5/variant1.py
+++ AI_Lab5/variant1.py
@@ -1,42 +1,87 @@
+# Import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError, SparseCategoricalCrossentropy
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from keras.datasets import mnist
 
-# Load MNIST dataset
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+# Load the dataset
+(X_train_full, y_train_full), (X_test, y_test) = mnist.load_data()
 
-# Normalize pixel values to [0, 1]
-train_images, test_images = train_images / 255.0, test_images / 255.0
+# Preprocess the data
+X_train_full = X_train_full.reshape((60000, 784)).astype('float32') / 255.0
+X_test = X_test.reshape((10000, 784)).astype('float32') / 255.0
+y_train_full = keras.utils.to_categorical(y_train_full)
+y_test = keras.utils.to_categorical(y_test)
 
+# Split the data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train_full, y_train_full, test_size=0.2, random_state=42)
 
-# Define neural network architecture
-def create_model(num_hidden_layers, width, loss_function):
-    model = Sequential()
-    model.add(Flatten(input_shape=(28, 28)))  # Flatten input images
-    for _ in range(num_hidden_layers):
-        model.add(Dense(width, activation='relu'))
-    model.add(Dense(10, activation='softmax'))  # Output layer with 10 classes
-    model.compile(optimizer='adam', loss=loss_function, metrics=['accuracy'])
-    return model
+# Define the MLP architecture
+model = keras.models.Sequential([
+    keras.layers.Dense(128, activation='relu', input_shape=(784,)),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
 
+# Compile the model
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd', metrics=['accuracy'])
 
 # Define hyperparameters
-learning_rates = [0.01, 0.001, 0.0001]
+learning_rates = [0.1, 0.01, 0.001]
 batch_sizes = [1, 32, 128]
 num_hidden_layers = [0, 1, 2]
-widths = [10, 50, 100]
-loss_functions = [MeanSquaredError(), MeanAbsoluteError(), SparseCategoricalCrossentropy(from_logits=True)]
+hidden_layer_widths = [64, 128, 256]
+loss_functions = ['mse', 'mae', 'categorical_crossentropy']
 
-# Train and evaluate models for all hyperparameter combinations
+# Train and evaluate the model for different hyperparameters
+results = []
 for lr in learning_rates:
     for batch_size in batch_sizes:
-        for num_layers in num_hidden_layers:
-            for width in widths:
-                for loss_func in loss_functions:
-                    model = create_model(num_layers, width, loss_func)
-                    model.fit(train_images, train_labels, batch_size=batch_size, epochs=10, verbose=0)
-                    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=0)
-                    print(
-                        f"lr={lr:.4f}, batch_size={batch_size}, num_layers={num_layers}, width={width}, loss_function={type(loss_func).__name__}: test_acc={test_acc:.4f}")
+        for num_hidden in num_hidden_layers:
+            for width in hidden_layer_widths:
+                for loss_fn in loss_functions:
+                    print("Training model with lr={}, batch_size={}, num_hidden={}, width={}, loss_fn={}".format(
+                        lr, batch_size, num_hidden, width, loss_fn))
+                    model = keras.models.Sequential()
+                    model.add(keras.layers.Dense(
+                        width, activation='relu', input_shape=(784,)))
+                    for i in range(num_hidden):
+                        model.add(keras.layers.Dense(width, activation='relu'))
+                    model.add(keras.layers.Dense(10, activation='softmax'))
+                    model.compile(loss=loss_fn, optimizer=keras.optimizers.SGD(
+                        lr=lr), metrics=['accuracy'])
+                    history = model.fit(
+                        X_train, y_train, epochs=10, batch_size=batch_size, validation_data=(X_valid, y_valid))
+                    val_accuracy = history.history['val_accuracy'][-1]
+                    results.append({'lr': lr, 'batch_size': batch_size, 'num_hidden': num_hidden,
+                                    'width': width, 'loss_fn': loss_fn, 'history': history,
+                                    'val_accuracy': val_accuracy})
+# Print results
+for result in results:
+    print(result)
+
+# Plot loss and accuracy curves
+
+for result in results:
+    plt.plot(result['history'].history['loss'], label='Training Loss')
+    plt.plot(result['history'].history['val_loss'], label='Validation Loss')
+    plt.title('Loss - LR={}, Batch Size={}, Num Hidden Layers={}, Width={}, Loss Function={}'.format(
+        result['lr'], result['batch_size'], result['num_hidden'], result['width'], result['loss_fn']))
+    plt.legend()
+    plt.show()
+
+plt.plot(result['history'].history['accuracy'], label='Training Accuracy')
+plt.plot(result['history'].history['val_accuracy'],
+         label='Validation Accuracy')
+plt.title('Accuracy - LR={}, Batch Size={}, Num Hidden Layers={}, Width={}, Loss Function={}'.format(
+    result['lr'], result['batch_size'], result['num_hidden'], result['width'], result['loss_fn']))
+plt.legend()
+plt.show()
+
+# Print best results
+best_result = max(results, key=lambda x: x['val_accuracy'])
+print("Best result:\n", best_result)
