f652a994b45e874bcd994345779ecdee9815d10b
Maria Koilalou <mariakoilalou@gmail.com>
2023-05-30 17:31:59 +0200

    lab 6 format fix

--- AI_Lab7/variant1.pl
+++ AI_Lab7/variant1.pl
@@ -1,3 +1,4 @@
+% Create an object for every month
 days_in_month(01, 31).
 days_in_month(02, 28).
 days_in_month(03, 31).
@@ -11,47 +12,53 @@ days_in_month(10, 31).
 days_in_month(11, 30).
 days_in_month(12, 31).
 
-
-add_date(Date, DaysToAdd, Result) :-
+% Add days
+add_date(Date, DaysToAdd) :-
+    % Extract the day and month from the input
     sub_string(Date, 0, 2, _, DayStr),
     sub_string(Date, 2, 2, _, MonthStr),
+    % Turn them into numbers
     atom_number(DayStr, Day),
     atom_number(MonthStr, Month),
+    % Add number of dates
     days_in_month(Month, DaysInMonth),
-    (   DaysToAdd + Day =< DaysInMonth
+    (   DaysToAdd + Day =< DaysInMonth  /* Don't have to go to next month */
     ->  NewDay is Day + DaysToAdd,
-        format_date(NewDay, Month, Result)
-    ;   RemainingDays is DaysToAdd - (DaysInMonth - Day + 1),
+        format_date(NewDay, Month)
+    ;   RemainingDays is DaysToAdd - (DaysInMonth - Day + 1), /* Has to go to next month */
         (   Month = 12
         ->  NextMonth is 1
         ;   NextMonth is Month + 1
         ),
-        format_date(1, NextMonth, NewDate),
-        add_date(NewDate, RemainingDays, NewResult),
-        Result = NewResult
+        format_date(1, NextMonth),
+        add_date("0106", RemainingDays)
     ).
 
-sub_date(Date, DaysToSubtract, Result) :-
+% Subtract days
+sub_date(Date, DaysToSubtract) :-
+    % Extract the day and month from the input
     sub_string(Date, 0, 2, _, DayStr),
     sub_string(Date, 2, 2, _, MonthStr),
+    % Turn them into numbers
     atom_number(DayStr, Day),
     atom_number(MonthStr, Month),
-    (   Day - DaysToSubtract > 0
+    % Subtract number of dates
+    (   Day - DaysToSubtract > 0    /* Don't have to go to the previous month */
     ->  NewDay is Day - DaysToSubtract,
-        format_date(NewDay, Month, Result)
-    ;   RemainingDays is DaysToSubtract - Day,
+        format_date(NewDay, Month)
+    ;   RemainingDays is DaysToSubtract - Day,  /* Has to go to the previous month */
         (   Month = 1
         ->  PrevMonth is 12
         ;   PrevMonth is Month - 1
         ),
         days_in_month(PrevMonth, DaysInPrevMonth),
         NewDay is DaysInPrevMonth - RemainingDays,
-        format_date(NewDay, PrevMonth, Result)
+        format_date(NewDay, PrevMonth)
     ).
 
-format_date(Day, Month, DateStr) :-
+% Formatted Output
+format_date(Day, Month) :-
     (Day < 10 -> atom_concat('0', Day, DayStr); atom_number(DayStr, Day)),
-    (Month < 10 -> atom_concat('0', Month, MonthStr); atom_number(MonthStr, 
-Month)),
-    atom_concat(DayStr, MonthStr, DateStr).
-
+    (Month < 10 -> atom_concat('0', Month, MonthStr); atom_number(MonthStr, Month)),
+    atom_concat(DayStr, MonthStr, DateStr),
+    format('"~w"',[DateStr]).
